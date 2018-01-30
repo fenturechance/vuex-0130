d630a1c2f03f6ff6a6d60a0e616771dd10eec54f
Jerry <fenturechance@gmail.com>
2018-01-30 16:35:13 +0800

    getter的概念

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,24 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-    <button @click="increase">+</button>
-    <p>{{ count }}</p>
-    <p>{{ localComputed }}</p>
+    <p>{{ doneTodosCount }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
   name: 'App',
   data() {
-      return {
-          localCount:5
-      }
+      return {}
   },
   created() {},
   computed:{
-    localComputed () {
-        return 5566
-    },
-    // 使用对象展开运算符将此对象混入到外部对象中
-    ...mapState({
-        count: state =>state.count
-    })
+    doneTodosCount () {
+        //state裡面done的資料只有一筆
+        return this.$store.state.todos.filter(todo => todo.done).length
+    }
   },
   methods: {
-    increase() {
-      this.$store.commit('increment');
-      console.log(this.$store.state.count);
-    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state: {
-        count: 0
-    },
-    mutations: {
-        increment(state) {
-            state.count++
-        }
+  state: {
+    todos: [{
+        id: 1,
+        text: '...',
+        done: true
+      },
+      {
+        id: 2,
+        text: '...',
+        done: false
+      }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
     }
+  }
+
 })
 
 export default store;
