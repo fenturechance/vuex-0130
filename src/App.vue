34ff89e727a43accf57ef992a80f0257734b7d23
Jerry <fenturechance@gmail.com>
2018-01-30 16:27:16 +0800

    mapState傳入陣列

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,6 @@
     <router-view/>
     <button @click="increase">+</button>
     <p>{{ count }}</p>
-    <p>{{ countAlias }}</p>
-    <p>{{ countPlusLocalState }}</p>
   </div>
 </template>
 
@@ -19,18 +17,10 @@ export default {
       }
   },
   created() {},
-  computed:mapState({
-    // 箭头函数可使代码更简练
-    count: state => state.count,
-
-    // 传字符串参数 'count' 等同于 `state => state.count`
-    countAlias: 'count',
-
-    // 为了能够使用 `this` 获取局部状态，必须使用常规函数
-    countPlusLocalState (state) {
-      return state.count + this.localCount
-    }
-  }),
+  computed:mapState([
+  // 映射 this.count 为 store.state.count
+  'count'
+]),
   methods: {
     increase() {
       this.$store.commit('increment');
