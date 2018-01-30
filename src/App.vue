78a4e0964cfed40e9da2931c50ea44cd77569ed1
Jerry <fenturechance@gmail.com>
2018-01-30 16:24:00 +0800

    vuex-commit-state

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <button @click="increase">+</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {},
+  computed: {
+      count() {
+          return this.$store.state.count;
+      }
+  },
+  methods: {
+    increase() {
+      this.$store.commit('increment');
+      console.log(this.$store.state.count);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
